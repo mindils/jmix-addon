17256900f3c89a01f2ef9bbd3f3981d4acd58fd1
mindils <mindils@mail.ru>
2024-03-18 23:12:44 +0300

    init

---
+++ app/src/main/java/com/company/app/security/DatabaseUserRepository.java
@@ -0,0 +1,29 @@
+package com.company.app.security;
+
+import com.company.app.entity.User;
+import io.jmix.securitydata.user.AbstractDatabaseUserRepository;
+import java.util.Collection;
+import org.springframework.context.annotation.Primary;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.stereotype.Component;
+
+@Primary
+@Component("UserRepository")
+public class DatabaseUserRepository extends AbstractDatabaseUserRepository<User> {
+
+    @Override
+    protected Class<User> getUserClass() {
+        return User.class;
+    }
+
+    @Override
+    protected void initSystemUser(final User systemUser) {
+        final Collection<GrantedAuthority> authorities = getGrantedAuthoritiesBuilder()
+                .addResourceRole(FullAccessRole.CODE)
+                .build();
+        systemUser.setAuthorities(authorities);
+    }
+
+    @Override
+    protected void initAnonymousUser(final User anonymousUser) {}
+}
